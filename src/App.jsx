5c4b1855658d3fccb09aa4874ee1e88c64fde214
Robin Karlsson <robinkarlsson2001@gmail.com>
2022-05-01 17:32:02 +0200

    Added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Container from 'react-bootstrap/Container'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import Navigation from './components/Navgiation'
 import Films from './pages/Films'
 import People from './pages/People'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
-
 const App = () => {
 
 	return (
 		<div id="App">
+      <Navigation />
+
 			<Container>
 				  <Routes>
               <Route path="/" element={<HomePage />} />
