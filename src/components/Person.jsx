ed9924a40d5265b96b6dfea41a68df0c29e22331
Robin Karlsson <robinkarlsson2001@gmail.com>
2022-05-02 17:31:38 +0200

    Added single Person page info and css for it

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -32,18 +32,82 @@ const Person = () => {
 
   return (
     <>
-        <h1>{person.name}</h1>
+        {error && {
+            error
+        }}
 
-        <div className="film-wrapper">
-          {films.map((film,index) => (
-            <ListGroup>
-              <ListGroup.Item
-                key = {index}
-                as={Link}
-                to={`/films/${GetURLId(film)}`}
-              >{`Film ${GetURLId(film)}`}</ListGroup.Item>
-            </ListGroup>
-          ))}  
+        {loading && (
+            <h1 className="text-center mt-5">Loading...</h1>
+        )}
+
+
+        <Card border="dark" className="mt-5">
+            <Card.Header
+            >People</Card.Header>
+            <Card.Body>
+            <Card.Title as="h1">{person.name}</Card.Title>
+            <hr />
+
+            <Card.Title>
+                <h4>General info</h4>
+            </Card.Title>            
+            <div className="info-wrapper-person">
+                <div>
+                      <Row>
+                        {`Birth Year: ${person.birth_year}`}
+                      </Row>
+
+                      <Row>
+                          {`Height: ${person.height}cm`}
+                      </Row>
+                      
+                      <Row>
+                          {`Weight: ${person.mass}kg`}
+                      </Row>
+                </div>
+                <div>
+                      <Row>
+                        {`Hair Color: ${person.hair_color}`}
+                      </Row>
+
+                      <Row>
+                          {`Eye Color: ${person.eye_color}`}
+                      </Row>
+                      
+                      <Row>
+                          {`Gender: ${person.gender}`}
+                      </Row>
+                </div> 
+            </div>
+            <hr />
+
+            <Card.Title>
+                <h4>Characters seen in this film</h4>
+            </Card.Title>
+            <hr />
+            <div className="films-characters-wrapper">
+                {films.map((film, index) => (
+                    <ListGroup key={index} className="col-4 ">
+                    <ListGroup.Item
+                        as={Link}
+                        to={`/film/${GetURLId(film)}`}
+                        className=""
+                    >{`Film ${GetURLId(film)}`}</ListGroup.Item>
+                    </ListGroup>
+                ))}  
+            </div>
+            
+            </Card.Body>
+        </Card>
+
+        <div>
+            <Button
+                as = {Link}
+                to = {`/people`}
+                className="back col-3 col-3 btn-dark"
+            >   
+                    Back
+            </Button>
         </div>
     </>
   )
